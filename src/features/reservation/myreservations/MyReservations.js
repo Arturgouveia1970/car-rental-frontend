b2877da8e2fb9458a472e4d4f74b8da83178aa60
Arturgouveia1970 <artgouveiag@gmail.com>
2022-12-06 15:03:53 +0100

    created initial UI pages

---
+++ src/features/reservation/myreservations/MyReservations.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MyReservations = () => (
+  <>
+    <h1>My Reservations</h1>
+    <h3>This page will show a list of my car Reservations</h3>
+  </>
+);
+
+export default MyReservations;
---
+++ src/features/reservation/reserve/Reserve.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Reserve = () => (
+  <>
+    <h1>Reserve Page</h1>
+    <h3>This page will be to reserve chosen car</h3>
+  </>
+);
+
+export default Reserve;
