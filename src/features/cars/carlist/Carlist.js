234da13fb1a1541f56c7f531a97da995153ff8d8
Pedro Marques <ppmarq1@gmail.com>
2022-12-08 08:51:16 +0000

    Create Carslist

--- src/features/cars/carlist/Carlist.js
+++ src/features/cars/carlist/Carlist.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import './Carlist.css';
+import { useNavigate } from 'react-router-dom';
+import CarCard from '../../../common/carcard/CarCard';
+import Sidebar from '../../../common/sidebar/Sidebar';
+import CarsListHeader from './CarsListHeader';
 
-const Cars = () => (
-  <>
-    <h1>Cars List</h1>
-    <h3>This page will show all cars available</h3>
-  </>
-);
+const Cars = () => {
+  const navigate = useNavigate();
+  const HandleDetails = (id) => navigate(`/Cars/${id}/details`);
+
+  return (
+    <div className="carslist-cont">
+      <Sidebar />
+      <div className="carslist-wrapper">
+        <carsListHeader />
+        <div className="car-cards-list">
+          {Array.from(Array(10).keys()).map((car) => (
+            <div
+              key={car}
+              className="car-card"
+              onClick={() => { HandleDetails(car); }}
+              aria-hidden="true"
+            >
+              <carCard car={car} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Cars;
