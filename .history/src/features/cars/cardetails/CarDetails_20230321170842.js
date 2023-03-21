86da4c426d13d9f9b5472581482e5b3d43b2cb07
arturgouveia1970 <artgouveiag@gmail.com>
2023-03-21 22:11:33 +0000

    working on reservations

---
+++ .history/src/features/cars/cardetails/CarDetails_20230321170842.js
@@ -0,0 +1,84 @@
+// eslint-plugin-react-hooks
+import React, { useEffect, useState, useCallback } from 'react';
+import { NavLink, useParams } from 'react-router-dom';
+import axios from 'axios';
+import styles from './CarDetails.module.css';
+// import Sidebar from '../../common/sidebar/Sidebar';
+
+const CarDetails = () => {
+  const { id } = useParams();
+  const [car, setCar] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  // fetch data callback function vvv asyncronous
+  const fetchData = useCallback(async (id) => {
+    setLoading(true);
+    // await axios get function
+    await axios
+      .get(`https://dreamcars.onrender.com/api/v1/cars/${id}`)
+      .then((response) => {
+        setCar(response.data);
+        // console.log(response.data);
+        setLoading(false);
+      });
+  }, []);
+  useEffect(() => {
+    // call the callback function
+    fetchData(id);
+
+    // include the function in the dependencies
+  }, [id, fetchData]);
+
+  // const newLocal = <Sidebar />;
+  if (loading) {
+    return (
+      <div className="container-fluid vh-100 v-100 d-flex justify-content-center align-items-center">
+        <i className="fa-solid fa-spinner fa-spin fs-1" />
+      </div>
+    );
+  }
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   axios
+  //     .get(`https://carrental2.onrender.com/api/v1/cars/${id}`)
+  //     .then((response) => {
+  //       setCar(response.data);
+  //       console.log(response.data);
+  //       setLoading(false);
+  //     });
+  // }, [id]);
+
+  return (
+    <>
+      {/* {newLocal} */}
+      <h2 className={styles.title}>CAR DETAILS</h2>
+      <div className={styles.detailContainer}>
+        <div style={{ backgroundImage:`url(${car}className={styles.car}>
+          <img src={car.image} alt={car.model} />
+        </div>
+        <div className={styles.detail}>
+          <h2>{car.model}</h2>
+          <div className={styles.carYear}>
+            <p>Year: </p>
+            <p>{car.year}</p>
+          </div>
+          <div className={styles.price}>
+            <p>Price per day: </p>
+            <p className={styles.priceColor}>
+              $
+              {car.price}
+            </p>
+          </div>
+          <NavLink to={`/reserve/${id}`} className={styles.btncontainer}>
+            <button type="button" className={styles.reservebtn}>
+              <i className="fa-solid fa-car-side me-3" />
+              Reserve
+            </button>
+          </NavLink>
+        </div>
+      </div>
+    </>
+  );
+};
+export default CarDetails;
---
+++ .history/src/features/reservation/myreservations/MyReservations_20230320173121.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
+// import { useSelector } from 'react-redux';
+import moment from 'moment';
+import { useParams } from 'react-router-dom';
+import styles from './MyReservations.module.css';
+
+const MyReservations = () => {
+  const { id } = useParams();
+  const [reservations, setReservations] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // const user = useSelector((state) => state.user);
+  const fetchdata = useCallback(async (id) => {
+    setLoading(true);
+    await axios
+      .get(`https://dreamcars.onrender.com/api/v1/reservations/${id}`
+        )
+        .then((response) => {
+          setReservations(response.data.reservations);
+          setLoading(false);
+        });
+    }, []);
+
+  if (reservations.length <= 0) {
+    if (loading) {
+      return (
+        <div className="container-fluid d-flex justify-content-center align-items-center vh-100">
+          <i className="fa-solid fa-spinner fa-spin fs-1" />
+        </div>
+      );
+    }
+    return (
+      <h2 className="mt-5 text-center">
+        You don&apos;t have any reservations yet.
+      </h2>
+    );
+  }
+  return (
+    <div className="container-fluid d-flex flex-column align-items-center">
+      <h2 className="my-4">My Reservations</h2>
+      <div className="d-flex flex-wrap align-items-center justify-content-center">
+        {reservations.map((res) => (
+          <div className={`${styles.card} d-flex m-4`} key={res.reservation.id}>
+            <img className={styles.img} src={res.car.picture} alt="car" />
+            <div className="ms-3 mt-1">
+              <h3 className={styles.car}>
+                <span>Car model: </span>
+                <span className="fw-bold">{res.car.model}</span>
+              </h3>
+              <p>
+                <span>Reservation date: </span>
+                <span className="fw-bold">
+                  {moment(res.reservation.date).format('MMMM Do YYYY')}
+                </span>
+              </p>
+              <p>
+                <span>Reservation city: </span>
+                <span className="fw-bold">{res.reservation.city}</span>
+              </p>
+              <p>
+                <span>Driver&apos;s name: </span>
+                <span className="fw-bold">{res.car.driver_name}</span>
+              </p>
+              <p>
+                <span>Price Per Day: </span>
+                <span className="fw-bold">
+                  $
+                  {res.car.price}
+                </span>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MyReservations;
