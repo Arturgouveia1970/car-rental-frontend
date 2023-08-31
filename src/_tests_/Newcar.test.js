34f8d35b7dfa12f966c2c134fba1b37570a32e3b
Arturgouveia1970 <artgouveiag@gmail>
2023-08-31 12:16:53 +0100

    implemented tests with jest library.

---
+++ src/_tests_/Newcar.test.js
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import Newcar from '../features/cars/newcar/Newcar';
+import store from '../Redux/configureStore';
+
+describe('New car form tests', () => {
+  it('renders correctly', () => {
+    const tree = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Newcar />
+        </BrowserRouter>
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('New car page has a "add" button', () => {
+    const register = render(
+      <>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Newcar />
+          </BrowserRouter>
+        </Provider>
+      </>,
+    );
+
+    expect(register.findByText('Add')).toBeTruthy();
+  });
+});
