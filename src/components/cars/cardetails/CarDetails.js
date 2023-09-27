7eddcbbd8bf474eb4a667dbcf4db30ec96cf5c4b
Artur <artgouveiag@gmail.com>
2023-09-27 22:35:44 +0100

    changed car details

--- src/components/cars/cardetails/CarDetails.js
+++ src/components/cars/cardetails/CarDetails.js
@@ -3,40 +3,11 @@ import React, { useEffect, useState } from 'react';
 import { NavLink, useParams } from 'react-router-dom';
 import axios from 'axios';
 import styles from './CarDetails.module.css';
-// import Sidebar from '../../common/sidebar/Sidebar';
 
 const CarDetails = () => {
   const { id } = useParams();
   const [car, setCar] = useState({});
   const [loading, setLoading] = useState(false);
-
-  // fetch data callback function vvv asyncronous
-  // const fetchData = useCallback(async (id) => {
-  //   setLoading(true);
-  //   // await axios get function
-  //   await axios
-  //     .get(`https://dreamcars2.onrender.com/api/v1/car/${id}`)
-  //     .then((response) => {
-  //       setCar(response.data);
-  //       // console.log(response.data);
-  //       setLoading(false);
-  //     });
-  // }, []);
-  // useEffect(() => {
-  //   // call the callback function
-  //   fetchData(id);
-
-  //   // include the function in the dependencies
-  // }, [id, fetchData]);
-
-  // // const newLocal = <Sidebar />;
-  // if (loading) {
-  //   return (
-  // <div className="container-fluid vh-100 v-100 d-flex justify-content-center align-items-center">
-  //       <i className="fa-solid fa-spinner fa-spin fs-1" />
-  //     </div>
-  //   );
-  // }
 
   useEffect(() => {
     axios
@@ -55,7 +26,6 @@ const CarDetails = () => {
 
   return (
     <>
-      {/* {newLocal} */}
       <h2 className={styles.title}>CAR DETAILS</h2>
       <div className={styles.detailContainer}>
         <div className={styles.car}>
